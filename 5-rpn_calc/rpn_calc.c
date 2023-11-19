82b440a634cac08d6a4c65619fd7437e38383221
pepedinho <2spii94@gmail.com>
2023-11-19 18:00:26 +0100

    rpn_calc(on progress)

--- 5-rpn_calc/rpn_calc.c
+++ 5-rpn_calc/rpn_calc.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include "rpn_calc.h"
 
-void ft_reset(int *a, int *b)
+//fonction pour restet la valeur de tab; 
+void ft_reset(int a, int b)
 {
-    *a = 0;
-    *b = 0;
+    a = 0;
+    b = 0;
 }
 
 int do_calcul(int a, int b, char op)
@@ -24,79 +25,99 @@ int do_calcul(int a, int b, char op)
     }
     else if (op == '/')
     {
-        if (b != 0)
-        {
-            return a / b;
-        }
-        else
-        {
-            printf("Error: Division by zero\n");
-            exit(1);
-        }
+        return a / b;
     }
     else
     {
-        printf("Error: Invalid operator\n");
-        exit(1);
+        return 0;
     }
 }
 
 int rpn_cal(char *str)
 {
     int i = 0;
+
     int tab[2];
-    int len = 0;
-    int nb_result = 0;
     
+    int len = 0;
+
+    int nb_result = 0;//variable qui stock le nombre de resultat intermiediaire nessecaire 
+    //recuperation de la longueur de str
     while (str[len] != '\0')
     {
         if (str[len] == '*' || str[len] == '+' || str[len] == '/' || str[len] == '-')
         {
             nb_result++;
         }
+        
         len++;
     }
     nb_result++;
-    
     char operator = '\0';
-    int *resultat = malloc(sizeof(int) * nb_result);
-    char buffer[10]; // Assuming the maximum number of digits in a number is 10
+
+    int *resutat = malloc(sizeof(int) * nb_result);// tableau pour stocker les resultat intermediaire et le resultat final
+    char *buffer = malloc(sizeof(char) * len);//buffer pour stocker les les chiffres composant le nombre
     int index = 0;
-    int op = 0;
-    int res_count = 0;
-    int step = 0;
-    
+
+    int op = 0;//represente l'index de l'operande contenue dans tab
+    int res_count = 0;//index du tableau de resultta
+    int step = 0; //indicateur de l'etape actuel
+    //boucle qui rempli le buffer et qui decoupe la chaine en operateur
     while (str[i] != '\0')
     {
-        if (str[i] >= '0' && str[i] <= '9' && str[i + 1] == ' ')
+        if (str[i] >= '0' && str[i] <= '9' && str[i +1] == ' ')
         {
             buffer[index] = str[i];
-            buffer[index + 1] = '\0';
-            tab[op] = atoi(buffer);
-            index = 0;
+            if(atoi(buffer) != 0)
+            {
+                if (buffer[index] == 0)
+                    buffer[index] = '\0';
+                else
+                    buffer[index + 1] = '\0';
+                
+                
+                tab[op] = atoi(buffer);
+                int count = 0;
+                printf("buffer = [");
+                while (buffer[count] != '\0')
+                {
+                    printf("%c", buffer[count]);
+                    count++;
+                }
+                printf("]\n");
+                
+                printf("debug op : tab[%d] = %d\n", op, atoi(buffer));
+                index = 0;
+            }
+
         }
-        else if (str[i] >= '0' && str[i] <= '9')
+        else if (str[i] >= '0' && str[i] <= '9')//si le caractere est un un chiffre on le stock dans buffer;
         {
             buffer[index] = str[i];
+            printf("debug buffer : buffer[%d] = %c \n", index, buffer[index]);
+            
             index++;
         }
-        else if (str[i] == ' ')
+        else if (str[i] == ' ')//si le caractere est un espace on on verifie si nou avon deja les operateur, si c'est le cas on proccede au calcul du resultat temporaire
         {   
             if (op == 2 && step == 0)
             {
-                resultat[res_count] = do_calcul(tab[op - 1], tab[op], operator);
+
+                resutat[res_count] = do_calcul(tab[op - 1], tab[op], operator);
+                printf(" l80 (etapes : %d): resultat[%d] = %d = (%d %c  %d)operator = '%c' \n", step,res_count, do_calcul(tab[op - 2], tab[op - 1], operator), tab[op - 2], operator, tab[op - 1], operator);
                 res_count++;
                 step++;
                 op = 0;
-                ft_reset(&tab[0], &tab[1]);
+                ft_reset(tab[0], tab[1]);
             }
             else if (op == 1 && step > 0)
             {
-                resultat[res_count] = do_calcul(resultat[res_count - 1], tab[0], operator);
+                resutat[res_count] = do_calcul(resutat[res_count - 1], tab[0], operator);
+                printf(" l88 (etapes : %d): resultat[%d] = %d = (res[res_count-1(%d)]%d %c  %d)operator = '%c' \n", step, res_count, do_calcul(resutat[res_count - 1], tab[0], operator), res_count - 1, resutat[res_count], operator, tab[0], operator);
                 res_count++;
                 step++;
                 op = 0;
-                ft_reset(&tab[0], &tab[1]);
+                ft_reset(tab[0], tab[1]);
             }
             op++;
         }
@@ -106,13 +127,15 @@ int rpn_cal(char *str)
         }
         else
         {
-            printf("Error: Invalid character\n");
-            exit(1);
-        } 
+            printf("Error!");
+            return 1;
+        }
+        
         i++;
     }
     
-    int res = resultat[res_count - 1];
-    free(resultat);
+    //free(buffer);
+    int res = resutat[res_count - 1];
+    free(resutat);
     return res;
 }
