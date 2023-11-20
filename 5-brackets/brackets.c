f86e2c5c89e1eaa38264892965990b8b569ca916
pepedinho <2spii94@gmail.com>
2023-11-20 17:11:33 +0100

    save

--- 5-brackets/brackets.c
+++ 5-brackets/brackets.c
@@ -1,8 +1,16 @@
 #include <unistd.h>
 
+int ft_strlen(char *str)
+{
+    int i = 0;
 
+    while (str[i] != '\0')
+        i++;
+    
+    return i;
+}
 
-int main(int argc, char const *argv[])
+int main(void)
 {
     int i = 0; 
     int open_par = 0;
@@ -14,78 +22,130 @@ int main(int argc, char const *argv[])
 
     int temp_open = 0;
     int temp_close = 0;
+    char str[] = "{(([]))}";
+    int len = ft_strlen(str);
     
     int open = open_bracket + open_cbrace + open_par;
     int close = close_bracket + close_cbrace + close_par;
 
-    while (argv[argc - 1][i] != '\0' )
+    while (str[i] != '\0' )
     {
         if (open != close)
         {
             int j = i - 1;
-            while (argv[argc - 1][j] != '\0')
+            int reg = j;
+            char buf = str[j];
+            while (j < len - reg)
             {
-
-
-               if (argv[argc - 1][j] == '(' || argv[argc - 1][j] == '{' || argv[argc - 1][j] == '[')
+               if (str[j] == '(' || str[j] == '{' || str[j] == '[')
                {
+                    if (str[j] == '(')
+                    {
+                        open_par++;
+                    }
+                    else if (str[j] == '{')
+                    {
+                        open_cbrace++;
+                    }
+                    else
+                    {
+                        open_bracket++;
+                    }
                     temp_open++;
                }
-               else if (argv[argc - 1][j] == ')' || argv[argc - 1][j] == '}' || argv[argc - 1][j] == ']')
+               else if (str[j] == ')' || str[j] == '}' || str[j] == ']')
                {
+                    if (str[j] == ')')
+                    {
+                        close_par++;
+                    }
+                    else if (str[j] == '}')
+                    {
+                        close_cbrace++;
+                    }
+                    else
+                    {
+                        close_bracket++;
+                    }
                     temp_close++;
                }
-               
 
-                if (temp_open == temp_close)
-                {
-                    
-                }
-                else
-                {
+               if (buf == '(' && str[j] == ')' && temp_open != temp_close) //a finir !!!
+               {
                     write(1, "Error", 5);
-                }
-            
+                    return 0;
+               }
+               else if (buf == '[' && str[j] == ']' && temp_open != temp_close)
+               {
+                    write(1, "Error", 5);
+                    return 0;
+               }
+               else if (buf == '{' && str[j] == '}' && temp_open != temp_close)
+               {
+                    write(1, "Error", 5);
+                    return 0;
+               }
+               
+               
+               
+               
                 j++;
             }
+            if (buf == '(' && close_par == open_par && temp_close == temp_open)
+            {
+                //write(1,"OK", 2);
+                open_bracket = 0;
+                open_cbrace = 0;
+                open_par = 0;
+                close_bracket = 0;
+                close_cbrace = 0;
+                close_par = 0;
+                temp_close = 0;
+                temp_open = 0;
+            }
+            else if (buf == '[' && close_bracket == open_bracket && temp_close == temp_open)
+            {
+                open_bracket = 0;
+                open_cbrace = 0;
+                open_par = 0;
+                close_bracket = 0;
+                close_cbrace = 0;
+                close_par = 0;
+                temp_close = 0;
+                temp_open = 0;
+            }
+            else if (buf == '{' && close_cbrace == open_cbrace && temp_open == temp_close)
+            {
+                open_bracket = 0;
+                open_cbrace = 0;
+                open_par = 0;
+                close_bracket = 0;
+                close_cbrace = 0;
+                close_par = 0;
+                temp_close = 0;
+                temp_open = 0;
+            }
+            else
+            {
+                write(1, "Error", 5);
+                return 0;
+                temp_close = 0;
+                temp_open = 0;
+            }
             
         }
 
-        if (argv[argc - 1][i] == '(' || argv[argc - 1][i] == '{' || argv[argc - 1][i] == '[')
+        if (str[i] == '(' || str[i] == '{' || str[i] == '[')
         {
-            if (argv[argc - 1][i] == '(')
-            {
-                open_par++;
-            }
-            else if (argv[argc - 1][i] == '{')
-            {
-                open_cbrace++;
-            }
-            else
-            {
-                open_bracket++;
-            }
-            open = open_bracket + open_cbrace + open_par;
+            open++;
         }
-        else if (argv[argc - 1][i] == ')' || argv[argc - 1][i] == '}' || argv[argc - 1][i] == ']')
+        else if (str[i] == ')' || str[i] == '}' || str[i] == ']')
         {
-            if (argv[argc - 1][i] == ')')
-            {
-                close_par++;
-            }
-            else if (argv[argc - 1][i] == '}')
-            {
-                close_cbrace++;
-            }
-            else
-            {
-                close_bracket++;
-            }
-            close = open_bracket + open_cbrace + open_par;
+            close = open;
         }
 
 
-        if ((argv[argc - 1][i] == '(' && argv[argc - 1][i + 1] == ')') || (argv[argc - 1][i] == '[' && argv[argc - 1][i + 1] == ']') || (argv[argc - 1][i] == '{' && argv[argc - 1][i + 1] == '}'))
+        if ((str[i - 1] == '(' && str[i] == ')') || (str[i - 1] == '[' && str[i] == ']') || (str[i - 1] == '{' && str[i] == '}'))
         {
             if (open == close)
             {
@@ -98,6 +158,8 @@ int main(int argc, char const *argv[])
 
         i++;
     }
+
+    write(1, "OK", 2);
     
     return 0;
 }
